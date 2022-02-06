6af07be655871f8b8e146bda99ebf30fb542fb7e
Max Koverdyaev <shadeglare@gmail.com>
2022-02-06 13:13:46 +0300

    [init] start

---
+++ Hexarc.Borsh/BorshSerializer.cs
@@ -0,0 +1,14 @@
+﻿namespace Hexarc.Borsh;
+
+public static class BorshSerializer
+{
+    public static Byte[] Serialize<TValue>(TValue value)
+    {
+        throw new NotSupportedException();
+    }
+
+    public static TValue Deserialize<TValue>(Byte[] bytes)
+    {
+        throw new NotImplementedException();
+    }
+}
