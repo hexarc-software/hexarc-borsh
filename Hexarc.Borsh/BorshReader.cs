1bc5d85467f4645c3ff663f3dd7594384870c965
Max Koverdyaev <shadeglare@gmail.com>
2022-02-12 10:55:14 +0300

    [cleanup]

--- Hexarc.Borsh/BorshReader.cs
+++ Hexarc.Borsh/BorshReader.cs
@@ -1,5 +1,4 @@
 using System.Runtime.CompilerServices;
-using Hexarc.Borsh.Serialization.Metadata;
 using static System.Buffers.Binary.BinaryPrimitives;
 
 namespace Hexarc.Borsh;
@@ -17,37 +16,37 @@ public ref struct BorshReader
     }
 
     public Boolean ReadBoolean() =>
-        this.ReadSpan(Constants.BooleanSize)[0] == 1;
+        this.ReadSpan(sizeof(Boolean))[0] == 1;
 
     public Byte ReadByte() =>
-        this.ReadSpan(Constants.ByteSize)[0];
+        this.ReadSpan(sizeof(Byte))[0];
 
     public SByte ReadSByte() =>
-        (SByte)this.ReadSpan(Constants.SByteSize)[0];
+        (SByte)this.ReadSpan(sizeof(SByte))[0];
 
     public Int16 ReadInt16() =>
-        ReadInt16LittleEndian(this.ReadSpan(Constants.Int16Size));
+        ReadInt16LittleEndian(this.ReadSpan(sizeof(Int16)));
 
     public UInt16 ReadUInt16() =>
-        ReadUInt16LittleEndian(this.ReadSpan(Constants.UInt16Size));
+        ReadUInt16LittleEndian(this.ReadSpan(sizeof(UInt16)));
 
     public Int32 ReadInt32() =>
-        ReadInt32LittleEndian(this.ReadSpan(Constants.Int32Size));
+        ReadInt32LittleEndian(this.ReadSpan(sizeof(Int32)));
 
     public UInt32 ReadUInt32() =>
-        ReadUInt32LittleEndian(this.ReadSpan(Constants.UInt32Size));
+        ReadUInt32LittleEndian(this.ReadSpan(sizeof(UInt32)));
 
     public Int64 ReadInt64() =>
-        ReadInt64LittleEndian(this.ReadSpan(Constants.Int64Size));
+        ReadInt64LittleEndian(this.ReadSpan(sizeof(Int64)));
 
     public UInt64 ReadUInt64() =>
-        ReadUInt64LittleEndian(this.ReadSpan(Constants.UInt64Size));
+        ReadUInt64LittleEndian(this.ReadSpan(sizeof(UInt64)));
 
     public Single ReadSingle() =>
-        ReadSingleLittleEndian(this.ReadSpan(Constants.SingleSize));
+        ReadSingleLittleEndian(this.ReadSpan(sizeof(Single)));
 
     public Double ReadDouble() =>
-        ReadDoubleLittleEndian(this.ReadSpan(Constants.DoubleSize));
+        ReadDoubleLittleEndian(this.ReadSpan(sizeof(Double)));
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private ReadOnlySpan<Byte> ReadSpan(Int32 size)
