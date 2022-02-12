904911b1c6efa49fa162ba535e3a39f65b78b861
Max Koverdyaev <shadeglare@gmail.com>
2022-02-12 17:47:43 +0300

    [minor]

--- Hexarc.Borsh/Option.cs
+++ Hexarc.Borsh/Option.cs
@@ -12,8 +12,11 @@ public sealed class Some<T> : Option<T> where T : class
 {
     public T Value { get; }
 
-    public Some(T? value) =>
+    internal Some(T? value) =>
         this.Value = value ?? throw new ArgumentException("Argument cannot be null", nameof(value));
 }
 
-public sealed class None<T> : Option<T> where T : class { }
+public sealed class None<T> : Option<T> where T : class
+{
+    internal None() {}
+}
