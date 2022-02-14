b828878d3490e257b858c80c8ce3188d33e882cc
Max Koverdyaev <shadeglare@gmail.com>
2022-02-14 14:39:27 +0300

    [feature] add simple class reader

--- Hexarc.Borsh.Tests/ClassSerializationTests.cs
+++ Hexarc.Borsh.Tests/ClassSerializationTests.cs
@@ -5,14 +5,21 @@ namespace Hexarc.Borsh.Tests;
 [TestFixture]
 public class ClassSerializationTests
 {
-    [TestCaseSource(nameof(PointCases))]
+    [TestCaseSource(nameof(SerializePointCases))]
     public void SerializePoint_ShouldMatchExpectation(Point point, Byte[] expected)
     {
         var result = BorshSerializer.Serialize(point);
         Assert.AreEqual(expected, result);
     }
 
-    private static Object[] PointCases =
+    [TestCaseSource(nameof(DeserializePointCases))]
+    public void DeserializePoint_ShouldMatchExpectation(Byte[] bytes, Point expected)
+    {
+        var result = BorshSerializer.Deserialize<Point>(bytes);
+        Assert.AreEqual(expected, result);
+    }
+
+    private static Object[] SerializePointCases =
     {
         new Object[]
         {
@@ -26,10 +33,39 @@ public class ClassSerializationTests
         }
     };
 
-    public sealed class Point
+    private static Object[] DeserializePointCases =
+    {
+        new Object[]
+        {
+            new Byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            new Point { X = 0, Y = 0, Z = 0 }
+        },
+        new Object[]
+        {
+            new Byte[] { 0, 16, 167, 69, 64, 152, 111, 200, 0, 32, 26, 69 },
+            new Point { X = 5346f, Y = -245345f, Z = 2466f }
+        }
+    };
+
+    public sealed class Point : IEquatable<Point>
     {
         public Single X { get; init; }
         public Single Y { get; init; }
         public Single Z { get; init; }
+
+        public Boolean Equals(Point? other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Math.Abs(this.X - other.X) <= 0.0000001f &&
+                   Math.Abs(this.Y - other.Y) <= 0.0000001f &&
+                   Math.Abs(this.Z - other.Z) <= 0.0000001f;
+        }
+
+        public override Boolean Equals(Object? obj) =>
+            ReferenceEquals(this, obj) || obj is Point other && Equals(other);
+
+        public override Int32 GetHashCode() =>
+            HashCode.Combine(X, Y, Z);
     }
 }
--- Hexarc.Borsh/Serialization/Converters/ObjectDefaultConverter.cs
+++ Hexarc.Borsh/Serialization/Converters/ObjectDefaultConverter.cs
@@ -5,40 +5,37 @@ namespace Hexarc.Borsh.Serialization.Converters;
 
 public class ObjectDefaultConverter<T> : BorshConverter<T> where T : notnull
 {
-    private readonly Action<BorshWriter, T>[] PropertyWriters;
+    private readonly TypeAccessor Accessor;
 
-    public ObjectDefaultConverter(BorshSerializerOptions options) =>
-        this.PropertyWriters = this.CreatePropertyWriters(options);
+    private readonly Dictionary<String, BorshConverter> Converters;
 
-    private Action<BorshWriter, T>[] CreatePropertyWriters(BorshSerializerOptions options)
+    public ObjectDefaultConverter(BorshSerializerOptions options)
     {
         var type = typeof(T);
-        var accessor = TypeAccessor.Create(type);
         var properties = type.GetProperties(BindingFlags.Instance | BindingFlags.Public);
-        return Array.ConvertAll(properties, property => this.CreatePropertyWriter(accessor, property, options));
-    }
 
-    private Action<BorshWriter, T> CreatePropertyWriter(
-        TypeAccessor accessor,
-        PropertyInfo propertyInfo,
-        BorshSerializerOptions options)
-    {
-        var type = propertyInfo.PropertyType;
-        var name = propertyInfo.Name;
-        var converter = options.GetConverter(type);
-        return (writer, value) => converter.WriteCoreAsObject(writer, accessor[value, name], options);
+        this.Accessor = TypeAccessor.Create(type);
+        this.Converters = Array
+            .ConvertAll(properties, p => (p.Name, Converter: options.GetConverter(p.PropertyType)))
+            .ToDictionary(x => x.Name, x => x.Converter);
     }
 
     public override void Write(BorshWriter writer, T value, BorshSerializerOptions options)
     {
-        foreach (var propertyWriter in this.PropertyWriters)
+        foreach (var (name, converter) in this.Converters)
         {
-            propertyWriter(writer, value);
+            converter.WriteCoreAsObject(writer, this.Accessor[value, name], options);
         }
     }
 
     public override T Read(ref BorshReader reader, BorshSerializerOptions options)
     {
-        throw new NotImplementedException();
+        var value = Activator.CreateInstance<T>();
+        foreach (var (name, converter) in this.Converters)
+        {
+            this.Accessor[value, name] = converter.ReadCoreAsObject(ref reader, options);
+        }
+
+        return value;
     }
 }
