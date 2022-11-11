08655187e0372fbf016936383a9bcaa0aedff697
Max Koverdyaev <shadeglare@gmail.com>
2022-11-11 12:33:19 +0300

    [net7] remove reflection for 128-bit primitives

--- Hexarc.Borsh/BinaryPrimitivesExt.cs
+++ Hexarc.Borsh/BinaryPrimitivesExt.cs
@@ -1,5 +1,4 @@
 using System.Buffers.Binary;
-using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -13,7 +12,10 @@ internal static class BinaryPrimitivesExt
     {
         if (!BitConverter.IsLittleEndian)
         {
-            var tmp = ReverseEndianness(value);
+            MemoryMarshal.Write(destination, ref value);
+            var tmp = ReverseEndiannessUInt128(
+                MemoryMarshal.Read<UInt64>(destination[..8]),
+                MemoryMarshal.Read<UInt64>(destination[8..]));
             MemoryMarshal.Write(destination, ref tmp);
         }
         else
@@ -21,13 +23,16 @@ internal static class BinaryPrimitivesExt
             MemoryMarshal.Write(destination, ref value);
         }
     }
-    
+
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void WriteInt128LittleEndian(Span<Byte> destination, Int128 value)
     {
         if (!BitConverter.IsLittleEndian)
         {
-            var tmp = ReverseEndianness(value);
+            MemoryMarshal.Write(destination, ref value);
+            var tmp = ReverseEndiannessUInt128(
+                MemoryMarshal.Read<UInt64>(destination[..8]),
+                MemoryMarshal.Read<UInt64>(destination[8..]));
             MemoryMarshal.Write(destination, ref tmp);
         }
         else
@@ -40,37 +45,22 @@ internal static class BinaryPrimitivesExt
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static UInt128 ReadUInt128LittleEndian(ReadOnlySpan<Byte> source) =>
         !BitConverter.IsLittleEndian
-            ? ReverseEndianness(MemoryMarshal.Read<UInt128>(source))
+            ? ReverseEndiannessUInt128(
+                MemoryMarshal.Read<UInt64>(source[..8]),
+                MemoryMarshal.Read<UInt64>(source[8..]))
             : MemoryMarshal.Read<UInt128>(source);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Int128 ReadInt128LittleEndian(ReadOnlySpan<Byte> source) =>
         !BitConverter.IsLittleEndian
-            ? ReverseEndianness(MemoryMarshal.Read<Int128>(source))
+            ? ReverseEndiannessInt128(
+                MemoryMarshal.Read<UInt64>(source[..8]),
+                MemoryMarshal.Read<UInt64>(source[8..]))
             : MemoryMarshal.Read<Int128>(source);
 
-    internal static Int128 ReverseEndianness(Int128 value) =>
-        (Int128)(ReverseEndiannessInt128.Invoke(null, new Object[] { value })
-                 ?? throw new InvalidOperationException());
+    internal static Int128 ReverseEndiannessInt128(UInt64 lower, UInt64 upper) =>
+        new(BinaryPrimitives.ReverseEndianness(lower), BinaryPrimitives.ReverseEndianness(upper));
 
-    internal static UInt128 ReverseEndianness(UInt128 value) =>
-        (UInt128)(ReverseEndiannessUInt128.Invoke(null, new Object[] { value })
-                  ?? throw new InvalidOperationException());
-
-    internal static readonly Type BinaryPrimitivesType = 
-        typeof(BinaryPrimitives);
-
-    internal static readonly MethodInfo ReverseEndiannessInt128 =
-        BinaryPrimitivesType.GetMethod(
-            "ReverseEndianness",
-            BindingFlags.Static | BindingFlags.NonPublic,
-            new[] { typeof(Int128) })
-        ?? throw new InvalidOperationException();
-
-    internal static readonly MethodInfo ReverseEndiannessUInt128 =
-        BinaryPrimitivesType.GetMethod(
-            "ReverseEndianness",
-            BindingFlags.Static | BindingFlags.NonPublic,
-            new[] { typeof(UInt128) })
-        ?? throw new InvalidOperationException();
+    internal static UInt128 ReverseEndiannessUInt128(UInt64 lower, UInt64 upper) =>
+        new(BinaryPrimitives.ReverseEndianness(lower), BinaryPrimitives.ReverseEndianness(upper));
 }
